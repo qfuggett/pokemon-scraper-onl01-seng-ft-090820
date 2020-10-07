09b5f9e9994062f829d6f79e8609f0c327db0a7f
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-07 18:38:05 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(array)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  
+  def self.save(name, type, db)
+    sql = <<-SQL
+    INSERT INTO pokemon (name, type) 
+    VALUES (?,?)
+    SQL
+    db.execute(sql, name, type)
+  end
+  
+  
+  def self.find(id, name, type)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE students.id = ?
+    SQL
+    pokemon = self.new(row[0], row[1],row[2])
+    pokemon
+  end 
 end
